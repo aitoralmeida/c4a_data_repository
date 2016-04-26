d182ee6619551399975a4d64c42c95a51785feff
Rubén <ruben.mulero@deusto.es>
2016-04-26 09:25:26 +0200

    - Added root detection on copy_to_tomcat_lib script.

--- LinkedDataInterface/scrips/copy_to_tomcat_lib.sh
+++ LinkedDataInterface/scrips/copy_to_tomcat_lib.sh
@@ -10,23 +10,31 @@ LIB="/var/lib/tomcat7/webapps/d2rq/WEB-INF/lib/"
 TOMCAT7="/usr/share/tomcat7/lib"
 
 
+# Test if you are root user
+if [ "$(id -u)" != "0" ]; then
+    echo "You need to run this script with sudo privileges"
+    exit 1
+fi
+
 # Test if tomcat7 lib folder exists in the system
 if [ ! -d $TOMCAT7 ]; then
     echo "Is tomcat7 installed in your system?"
     exit 1
 fi
 
+# Test if $LIB exist
 if [ -d $LIB ] && [ "$(ls -A $LIB)" ]; then
      # Copy contents to Tomcat's lib
      for dest in `ls $LIB`
      do
-        cp $LIB/$dest $TOMCAT7
+        mv $LIB/$dest $TOMCAT7
      done
      echo "Libs copied successfully!!"
      # Restart tomcat server
      service tomcat7 restart
 else
-    echo "Destination d2rq installation not exists or  there isn't any lib to copy"
+    echo "There isn't any installation of d2rq. Please make sure you have deployed anything in the system."
     exit 1
 fi
+
 exit 0
