18d14c9435fe680f0bdae1e9b4172ccc18e39387
Rubén <ruben.mulero@deusto.es>
2016-10-03 16:43:09 +0200

    - Some modifications in ruleEngine Package to use in the Test server - Some modifications in the Tests Class - New Rules added

--- LinkedDataInterface/src/ruleEngine/test/eu/deustotech/city4age/RuleEngineTest.java
+++ LinkedDataInterface/src/ruleEngine/test/eu/deustotech/city4age/RuleEngineTest.java
@@ -9,12 +9,21 @@ import com.hp.hpl.jena.reasoner.ValidityReport;
 import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasoner;
 import com.hp.hpl.jena.reasoner.rulesys.Rule;
 import com.hp.hpl.jena.util.FileManager;
+import com.sun.javafx.util.Logging;
 import de.fuberlin.wiwiss.d2rq.jena.ModelD2RQ;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.Assert.*;
 
@@ -24,6 +33,8 @@ import static org.junit.Assert.*;
  * @author Rubén Mulero
  */
 public class RuleEngineTest {
+
+    private static final Logger LOGGER = Logger.getLogger( InferenceMain.class.getName() );
 
     private final OntModel finalResult = ModelFactory.createOntologyModel();
     private Model instances;
@@ -66,12 +77,18 @@ public class RuleEngineTest {
      */
     @Test
     public void testValidRulesFile() throws Exception {
-        //todo find a good solution to know if there are rules loaded
-        assertTrue(true);
+        boolean res = false;
+        // We are going to load rules from file
+        List<Rule> listOfRules = Rule.rulesFromURL("file:./rules.txt");
+        if (listOfRules.size() > 0) {
+            // There are rules loaded into the list, so the file contains valid rules
+            res = true;
+        }
+        assertTrue(res);
     }
 
     /**
-     * Test if an infefence model produce any results
+     * Test if an inference model produce any results
      *
      */
     @Test
@@ -109,11 +126,69 @@ public class RuleEngineTest {
      */
     @Test
     public void testNewStatements() throws Exception {
+        boolean res = false;
         InfModel inf = ModelFactory.createInfModel(this.myReasoner, this.instances);
         if (!inf.isEmpty()) {
             // It returns True if empty
-           assertTrue(!inf.getDeductionsModel().isEmpty());
+           res = !inf.getDeductionsModel().isEmpty();
         }
+        assertTrue(res);
+    }
+
+    /**
+     * Test is new infered data is uploaded into Fuseki server. To do this it is needed.
+     *
+     *          --> A Fuseki server runing in localhost int port 3030
+     *          ---> A city4age dataset created into Fuseki Server
+     *
+     */
+    @Test
+    public void testUploadedData() throws Exception {
+        boolean res = false;
+        InfModel inf = ModelFactory.createInfModel(this.myReasoner, this.instances);
+        final OntModel finalResult = ModelFactory.createOntologyModel();
+
+
+        finalResult.add(this.instances);
+        finalResult.add(inf.getDeductionsModel());
+        // Set prefix map
+        finalResult.setNsPrefixes(this.instances.getNsPrefixMap());
+        finalResult.setNsPrefixes(inf.getDeductionsModel().getNsPrefixMap());
+
+        // Converting final result to string and writ it
+        StringWriter out = new StringWriter();
+        finalResult.write(out, "RDF/XML");
+        String result = out.toString();
+        // Launch curl to upload or current knowledge into Fuseki
+        ProcessBuilder p = new ProcessBuilder("curl", "-k", "-X", "POST", "--header", "Content-Type: application/rdf+xml",
+                "-d", result, "http://localhost:3030/city4age/data");
+        try {
+            System.out.println("Uploading new Knowledge to Fuseki......................");
+            System.out.println("");
+            // Execute our command
+            final Process shell = p.start();
+            // catch output and see if all is ok
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(shell.getInputStream()));
+            StringBuilder builder = new StringBuilder();
+            String line = null;
+            while ( (line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append(System.getProperty("line.separator"));
+            }
+            String output = builder.toString();
+            if (output.length() > 0 && output.contains("count")) {
+                // We have good response from the server
+                res = true;
+            }else {
+                System.err.println("Some error happened. Is Fuseki activated?");
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Asserting the result
+        assertTrue(res);
     }
 
 }
